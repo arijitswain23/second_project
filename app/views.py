52fa8af93181bcefe96671169256cf42d031609d
arijitswain23 <arijitswain369@gmail.com>
2023-11-08 15:49:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def silk(request):
+    return HttpResponse ("<h1><marquee>Dairymilk Silk</marquee></h1>")
+
+# Create your views here.
